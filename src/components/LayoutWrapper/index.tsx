240d9e942addc24ba64a294e4dce13f5f0b335c1
sabinbaniya <dummymail@tutanota.de>
2024-01-31 21:25:02 +0545

    feat: create Layout wrapper

---
+++ src/components/LayoutWrapper/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { atom, useAtom } from "jotai";
+import { ReactNode, useEffect } from "react";
+import { Socket, io } from "socket.io-client";
+
+const _socketAtom = atom<Socket | null>(null);
+
+export const useSocket = () => {
+  const [socket] = useAtom(_socketAtom);
+  return socket;
+};
+
+export const LayoutWrapper = ({ children }: { children: ReactNode }) => {
+  const [, setSocket] = useAtom(_socketAtom);
+
+  useEffect(() => {
+    const socket = io(process.env.SOCKET_URL!, {
+      // path: "/",
+      transports: ["websocket"],
+    });
+
+    setSocket(socket);
+
+    // console.log(socket);
+    return () => {
+      socket.close();
+      setSocket(null);
+    };
+  }, [setSocket]);
+
+  return <>{children}</>;
+};
